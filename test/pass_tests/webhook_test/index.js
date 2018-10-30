5e130e2b83f7fb0f1d84fb56876fb9bbe424a95d
Stéfano Stypulkowski Zanata <made.in.stefano@gmail.com>
2018-10-30 21:06:37 +0000

    Fixed tests

--- test/pass_tests/webhook_test/index.js
+++ test/pass_tests/webhook_test/index.js
@@ -10,15 +10,15 @@ module.exports = {
     logger.log( 'Sending a get to the server url passing value 1' );
     axios.get( `${env.serverUrl}?data=${values[0]}` );
 
-    logger.log( 'Sending a get to the server url passing value 2' );
-    axios.get( `${env.serverUrl}?data=${values[1]}` );
-
     const { qs: qs1 } = await env.asyncEvent( 'http-get' );
     logger.ok( 'Received value one' );
 
     if ( qs1.data !== values[0] ) {
       throw new Error( `Query string data for the first call should be ${values[0]} but ${qs1.data} was received.` );
     }
+
+    logger.log( 'Sending a get to the server url passing value 2' );
+    axios.get( `${env.serverUrl}?data=${values[1]}` );
 
     const { qs: qs2 } = await env.asyncEvent( 'http-get' );
     logger.ok( 'Received value two' );
